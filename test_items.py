e25ea742adccb362f3ee4903cf2047f2a770773a
s-lekomtsev <lekomtsevsa@gmail.com>
2022-11-05 00:15:57 +0500

    Final
    Добавил ожидание

--- test_items.py
+++ test_items.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.implicitly_wait(30)
+    time.sleep(30)
     message1 = browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
 
     assert message1.text != ""
