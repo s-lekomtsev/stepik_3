defa21438e6f7e9390302fe17c5428e28c8cc95f
s-lekomtsev <lekomtsevsa@gmail.com>
2022-11-04 23:32:16 +0500

    work ver
    Добавил печать сообщения.

---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.implicitly_wait(30)
+    message1 = browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+
+    assert message1.text != ""
+    print("Нашли кнопку: " + message1.text)
