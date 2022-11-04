7b09f6f1fbc49d601d1c08cfb36952abd17b62da
s-lekomtsev <lekomtsevsa@gmail.com>
2022-11-05 00:28:57 +0500

    name
    Сменил имя

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_basket_button(browser):
     browser.get(link)
     time.sleep(30)
     message1 = browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
